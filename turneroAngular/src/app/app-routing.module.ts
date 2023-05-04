f7d8afecab463ace0c10bcd9dcbeb6061cb3f346
Seba Olima <edificarvcp@gmail.com>
2023-05-03 21:39:30 -0300

    creando componente de contacto

--- turneroAngular/src/app/app-routing.module.ts
+++ turneroAngular/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './componentes/login/login.component';
 import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { ContactosComponent } from './pages/contactos/contactos.component';
 
 
 const routes: Routes = [
   
   { path: "login", component: LoginComponent, pathMatch: "full" },
-  { path: "Servicios", component: ServiciosComponent }
+  { path: "Servicios", component: ServiciosComponent },
+  { path: "Contactos", component: ContactosComponent}
   
 ];
 
