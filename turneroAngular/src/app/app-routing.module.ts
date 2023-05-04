8e971e85555ddcc329234b3389e92c626d1b994e
Vero <veronievasguardia@gmail.com>
2023-05-03 21:04:19 -0300

    creando componente de registro y vinculando con login

--- turneroAngular/src/app/app-routing.module.ts
+++ turneroAngular/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './componentes/login/login.component';
 import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
 
 
 const routes: Routes = [
   
   { path: "login", component: LoginComponent, pathMatch: "full" },
-  { path: "Servicios", component: ServiciosComponent }
+  { path: "Servicios", component: ServiciosComponent },
+  { path: "Registro", component: RegistroComponent}
   
 ];
 
