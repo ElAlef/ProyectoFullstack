a7dcc26ff558e43b0ad6399e6561a6618a294c6a
Alejandro <alebenitez.89@gmail.com>
2023-05-17 21:18:57 -0300

    subiendo proyecto django con la migracion y conexin al servidor

--- Backend/TurneroRF/asgi.py
+++ Backend/TurneroRF/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for drf project.
+ASGI config for TurneroRF project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TurneroRF.settings')
 
 application = get_asgi_application()
--- Backend/TurneroRF/wsgi.py
+++ Backend/TurneroRF/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for drf project.
+WSGI config for TurneroRF project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TurneroRF.settings')
 
 application = get_wsgi_application()
