4ed8e1e701c608e282bf199e36fa55c694f2c293
Alejandro <alebenitez.89@gmail.com>
2023-06-03 20:18:39 -0300

    corriendo errores de tipeo en views.py

--- Django/api/views.py
+++ Django/api/views.py
@@ -6,47 +6,48 @@ from rest_framework import viewsets
 from .serializer import EspecialidadSerializer
 from .models import Especialidad
 
-from .serializer import EspecialistaSerializer
-from .models import Especialista
-
-from .serializer import HorarioDeAtencionSerializer
-from .models import HorarioDeAtencion
-
-from .serializer import turnosPorEspecialistaSerializer
-from .models import turnosPorEspecialista
-
-from .serializer import PacienteSerializer
-from .models import Paciente
-
-from .serializer import ReservaDeTurnoSerializer
-from .models import ReservaDeTurno
-
-from .serializer import PagoSerializer
-from .models import Pago
 class EspecialidadViewSet(viewsets.ModelViewSet):
     queryset=Especialidad.objects.all()
     serializer_class= EspecialidadSerializer
+
+from .serializer import EspecialistaSerializer
+from .models import Especialista
 
 class EspecialistaViewSet(viewsets.ModelViewSet):
     queryset=Especialista.objects.all()
     serializer_class= EspecialistaSerializer
 
+from .serializer import HorarioDeAtencionSerializer
+from .models import HorarioDeAtencion
+
 class HorarioDeAtencionViewSet(viewsets.ModelViewSet):
     queryset=HorarioDeAtencion.objects.all()
-    serializer_class= HorarioDeAtencionSerializer    
+    serializer_class= HorarioDeAtencionSerializer   
+
+from .serializer import turnosPorEspecialistaSerializer
+from .models import turnosPorEspecialista
 
 class turnoPorEspecialistaViewSet(viewsets.ModelViewSet):
     queryset=turnosPorEspecialista.objects.all()
-    serializer_class= turnosPorEspecialistaSerializer    
+    serializer_class= turnosPorEspecialistaSerializer 
+
+from .serializer import PacienteSerializer
+from .models import Paciente
 
 class PacienteViewSet (viewsets.ModelViewSet):
     queryset=Paciente.objects.all()
     serializer_class= PacienteSerializer 
 
+from .serializer import RevervaDeTurnoSerializer
+from .models import ReservaDeTurno
+
 class ReservaDeTurnoViewSet(viewsets.ModelViewSet):
     queryset=ReservaDeTurno.objects.all()
-    serializer_class=ReservaDeTurnoSerializer
+    serializer_class= RevervaDeTurnoSerializer
+
+from .serializer import PagoSerializer
+from .models import Pago
 
 class PagoViewSet(viewsets.ModelViewSet):
     queryset=Pago.objects.all()
-    serializer_class=PagoSerializer
+    serializer_class= PagoSerializer
