ff5351359d828ef5af3693803ebfe3ddab20a5ed
Vero <veronievasguardia@gmail.com>
2023-06-04 08:56:55 -0300

    cambiando urls y probando el crud en api rest

--- Django/api/urls.py
+++ Django/api/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
 
-<<<<<<< HEAD
-from api import views
-=======
 #from Especialidad.views import EspecialidadViewSet
 from api import views
 
@@ -13,14 +10,6 @@ router.register(r'Especialista',views.EspecialistaViewSet)
 router.register(r'HorarioDeAtencion',views.HorarioDeAtencionViewSet)
 router.register(r'turnosPorEspecialista',views.turnoPorEspecialistaViewSet)
 
-#from Especilaidad.views import EspecialidadViewSet
-from turneroWeb  import views
->>>>>>> RamaAlejandro
-
-router= routers.DefaultRouter()
-router.register(r'Especialidad',views.EspecialidadViewSet)
-
-#----
 urlpatterns = [
      path('', include(router.urls)),
 ]
