daa006ca095c4a09234b6768e08a9b23f03ab76e
Alejandro <alebenitez.89@gmail.com>
2023-06-17 17:58:22 -0300

    agregando modelos al serializador

--- backend2023/api/turneroapp/serializers.py
+++ backend2023/api/turneroapp/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
 
+from .models import Especialidad
+from .models import Especialista
+from .models import HorarioDeAtencion
+from .models import turnosPorEspecialista
+from .models import Paciente
+from .models import ReservaDeTurno
+from .models import Pago
+
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
         required=True)
@@ -19,3 +27,41 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('email', 'nombre', 'apellido', 'fechaNacimiento', 'dni', 'username', 'password')
     def validate_password(self, value):
         return make_password(value)
+
+class EspecialidadSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model= Especialidad
+        fields='__all__'
+        # fields=('nombre')
+
+class EspecialistaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Especialista
+        fields='__all__'
+        # fields=('nombre')
+
+class HorarioDeAtencionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= HorarioDeAtencion
+        fields='__all__'
+
+class turnosPorEspecialistaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= turnosPorEspecialista
+        fields='__all__' 
+
+class PacienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Paciente
+        fields='__all__'
+
+class RevervaDeTurnoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= ReservaDeTurno
+        fields='__all__'
+
+class PagoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Pago
+        fields='__all__'              
