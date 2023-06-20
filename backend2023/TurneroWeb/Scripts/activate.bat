7be8e8c39fcf0764bf2906f7f65ef29ef04b928f
Alejandro <alebenitez.89@gmail.com>
2023-06-20 12:08:26 -0300

    actualizando proyecto

--- backend2023/TurneroWeb/Scripts/activate.bat
+++ backend2023/TurneroWeb/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Alejandro\Desktop\TURNERO\ProyectoFullstack\backend2023\TurneroWeb
+set VIRTUAL_ENV=C:\Users\Alejandro\Desktop\ispc\ProyectoFullstack\backend2023\TurneroWeb
 
 if not defined PROMPT set PROMPT=$P$G
 
