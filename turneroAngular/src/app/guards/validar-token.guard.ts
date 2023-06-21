bbabdd4bde9c518c58f1d7cecd3d5469fc03042c
Alejandro <alebenitez.89@gmail.com>
2023-06-21 00:56:08 -0300

    resolviendo conflicto de merge en validator y validar-token

--- turneroAngular/src/app/guards/validar-token.guard.ts
+++ turneroAngular/src/app/guards/validar-token.guard.ts
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { Injectable } from '@angular/core';
 import { CanActivate, CanLoad } from '@angular/router';
 import { Observable } from 'rxjs';
@@ -20,48 +18,5 @@ export class ValidarTokenGuard implements CanActivate, CanLoad {
     return this.authService.validarToken()
   
   }
-=======
-import { Injectable } from '@angular/core';
-import { CanActivate, CanLoad } from '@angular/router';
-import { Observable } from 'rxjs';
-import { AuthService } from '../auth/services/auth.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class ValidarTokenGuard implements CanActivate, CanLoad {
-
-  constructor ( private authService: AuthService ) {}
-
-  canActivate(): Observable<boolean>  | boolean  {
-    return this.authService.validarToken()
-    
-  }
-  canLoad(): Observable<boolean> | boolean  {
-    return this.authService.validarToken()
-  
-  }
->>>>>>> 44b1b6363da120ebb924d13b3c4926848f63789d
-=======
-import { Injectable } from '@angular/core';
-import { CanActivate, CanLoad } from '@angular/router';
-import { Observable } from 'rxjs';
-import { AuthService } from '../auth/services/auth.service';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class ValidarTokenGuard implements CanActivate, CanLoad {
-
-  constructor ( private authService: AuthService ) {}
-
-  canActivate(): Observable<boolean>  | boolean  {
-    return this.authService.validarToken()
-    
-  }
-  canLoad(): Observable<boolean> | boolean  {
-    return this.authService.validarToken()
-  
-  }
->>>>>>> main
 }
